4e2d5bc2974dfe93c6c887c7032f3269a42d6ecd
Darklordchinchin <18554@students.mrgs.school.nz>
2021-06-11 09:34:20 +1200

    first commit

--- app/src/main/java/com/bananaemperor/myapplication/MainActivity.java
+++ app/src/main/java/com/bananaemperor/myapplication/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView quest_list;
+    EditText QuestEntry;
 
 
 
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText QuestEntry =(EditText)findViewById(R.id.QuestEntry);
+         QuestEntry =(EditText)findViewById(R.id.QuestEntry);
         quest_list = (ListView)findViewById(R.id.Quest_List);
 
         ArrayList<String> arrayList=new ArrayList<>();
@@ -28,8 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
 
-        quest_list.setAdapter(arrayAdapter);
-        
+        //quest_list.setAdapter(arrayAdapter);
+
+
+
+        Button add_quest = (Button)findViewById(R.id.add_quest);
+        add_quest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String quest = QuestEntry.getText().toString();
+                arrayList.add(quest);
+                quest_list.setAdapter(arrayAdapter);
+
+            }
+        });
+
 
 
 
