c0f62df4979cbd66c2ac357baeeb3585c1bd23c8
Darklordchinchin <18554@students.mrgs.school.nz>
2021-07-16 18:30:33 +1200

    first commit

--- app/src/main/java/com/bananaemperor/myapplication/MainActivity.java
+++ app/src/main/java/com/bananaemperor/myapplication/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.bananaemperor.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static android.widget.Toast.*;
+
 public class MainActivity extends AppCompatActivity {
+    // defines the various variables that I'll need
     ListView quest_list;
     EditText QuestEntry;
 
@@ -23,25 +28,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         QuestEntry =(EditText)findViewById(R.id.QuestEntry);
+         QuestEntry =(EditText)findViewById(R.id.QuestEntry); //Link the with their Id
         quest_list = (ListView)findViewById(R.id.Quest_List);
 
-        ArrayList<String> arrayList=new ArrayList<>();
-         arrayList.add("test");
+        ArrayList<String> arrayList=new ArrayList<>(); //Create an array list for the list view
 
+        //Create the adapter for the list view
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
 
-        //quest_list.setAdapter(arrayAdapter);
-
-
+        //Create button for adding quest
 
         Button add_quest = (Button)findViewById(R.id.add_quest);
         add_quest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String quest = QuestEntry.getText().toString();
-                arrayList.add(quest);
+                arrayList.add(quest);                       //pull the text entered into the edit text
                 quest_list.setAdapter(arrayAdapter);
+                QuestEntry.getText().clear();
+                DatabaseHelper MyDB = new DatabaseHelper(MainActivity.this);  //Links this to the database
+                MyDB.add_quest(QuestEntry.getText().toString().trim());     //Sends the quest from the edit text to the database
 
             }
         });
